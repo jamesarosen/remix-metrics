780e67cc574f0f1b88ae644969f28d2f628b9e42
James A Rosen <james.a.rosen@gmail.com>
2022-08-20 21:57:37 -0700

    ✨(metric/graph) minimum viable graph

--- app/routes/metrics/cosine/graph.tsx
+++ app/routes/metrics/cosine/graph.tsx
@@ -1,3 +1,14 @@
+import { LineChart, Line, XAxis, YAxis } from "recharts";
+import { useMetricContext } from "~/util/MetricContext";
+
 export default function Graph() {
-  return 'nothing here yet'
+  const metric = useMetricContext()
+
+  return (
+    <LineChart width={500} height={300} data={metric.data}>
+      <XAxis dataKey="timestamp" />
+      <YAxis />
+      <Line type="monotone" dataKey="value" stroke="#8884d8" />
+    </LineChart>
+  );
 }
