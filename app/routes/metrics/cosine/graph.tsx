6439025b11fe442e0e84e844fabdce160608c36e
James A Rosen <james.a.rosen@gmail.com>
2022-08-21 08:44:48 -0700

    🚸(Graph) *further* improve X-axis readability

--- app/routes/metrics/cosine/graph.tsx
+++ app/routes/metrics/cosine/graph.tsx
@@ -1,8 +1,7 @@
-import { LineChart, Line, XAxis, YAxis } from "recharts";
+import { LineChart, Line, Text, XAxis, YAxis } from "recharts";
 import { useMetricContext } from "~/util/MetricContext";
 
-const X_AXIS_TICK_COUNT = 10;
-const firstTickFormatter = new Intl.DateTimeFormat("en-US", {
+const longTickFormatter = new Intl.DateTimeFormat("en-US", {
   year: "numeric",
   month: "numeric",
   day: "numeric",
@@ -11,23 +10,23 @@ const firstTickFormatter = new Intl.DateTimeFormat("en-US", {
   second: "numeric",
   hour12: false,
 });
-const lastTickFormatter = new Intl.DateTimeFormat("en-US", {
+const shortTickFormatter = new Intl.DateTimeFormat("en-US", {
   hour: "numeric",
   minute: "numeric",
   second: "numeric",
   hour12: false,
 });
 
-function formatXAxisTick(value: string, index: number) {
-  if (index === 0) {
-    return firstTickFormatter.format(parseInt(value, 10));
+function XAxisTick({ payload: { index, value }, count, ...props }: any) {
+  if (index === 0 || index === count - 1) {
+    return <Text {...props}>{shortTickFormatter.format(value)}</Text>
   }
 
-  if (index === X_AXIS_TICK_COUNT - 1) {
-    return lastTickFormatter.format(parseInt(value, 10));
+  if (index === count / 2) {
+    return <Text {...props}>{longTickFormatter.format(value)}</Text>
   }
 
-  return "";
+  return <></>
 }
 
 export default function Graph() {
@@ -39,10 +38,10 @@ export default function Graph() {
         dataKey="timestamp"
         interval={0}
         padding={{ left: 50, right: 50 }}
-        tickCount={X_AXIS_TICK_COUNT}
-        tickFormatter={formatXAxisTick}
+        tick={<XAxisTick count={metric.data.length} />}
+        tickMargin={6}
       />
-      <YAxis />
+      <YAxis tickMargin={6} />
       <Line type="monotone" dataKey="value" stroke="#8884d8" />
     </LineChart>
   );
