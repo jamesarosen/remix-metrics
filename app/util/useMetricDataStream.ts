967311399b9d61f0c3360539dca6791e61f04384
James A Rosen <james.a.rosen@gmail.com>
2022-08-21 12:51:15 -0700

    ✨(MetricContext) use server-generated streaming data

---
+++ app/util/useMetricDataStream.ts
@@ -0,0 +1,23 @@
+import type { MetricDatum } from '~/data/metrics/types';
+import { useEffect } from 'react'
+
+type Callback = (data: Array<MetricDatum>) => void
+
+export default function useMetricDataStream(since: number, onNewData: Callback) {
+  useEffect(() => {
+    if (since == null || isNaN(since)) {
+      return
+    }
+
+    function handler(event: MessageEvent) {
+      onNewData(JSON.parse(event.data))
+    }
+
+    const eventSource = new EventSource(`/metrics/cosine/stream?since=${since}`);
+    eventSource.addEventListener('metric', handler);
+
+    return () => {
+      eventSource.removeEventListener('metric', handler)
+    }
+  }, [since, onNewData])
+}
