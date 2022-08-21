b8c5d5fb4d707153f67c575500385ff1e414ae19
James A Rosen <jamesr@everlane.com>
2022-08-20 21:14:25 -0700

    🚸(Cosine) make the period more human-scale
    Previously, the period was so short that the cosine function appeared
    more like a generator of random values in [-1, 1]. This slows it down
    to a 10s period, which shows the cosine nicely.

--- app/data/metrics/Cosine.ts
+++ app/data/metrics/Cosine.ts
@@ -1,5 +1,12 @@
 import type { Metric, MetricDatum } from './types'
 import secondsInRange from "~/util/secondsInRange"
+
+// The period, in milliseconds, of the cosine wave
+const PERIOD = 10000
+
+function value(timestamp: number): number {
+  return Math.cos(2 * Math.PI / PERIOD * timestamp)
+}
 export default class Cosine implements Metric {
   name = 'Cosine'
   start: number
@@ -10,6 +17,6 @@ export default class Cosine implements Metric {
     this.start = start
     this.end = end
     this.data = secondsInRange(start, end)
-      .map(timestamp => ({ timestamp, value: Math.cos(timestamp) }))
+      .map(timestamp => ({ timestamp, value: value(timestamp) }))
   }
 }
