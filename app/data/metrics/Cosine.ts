00eba8fdad1547786f701390d7f2525801d69def
James A Rosen <james.a.rosen@gmail.com>
2022-08-20 20:01:29 -0700

    ✨ (MetricContext) add streaming data
    <MetricContext.Provider> now uses `setTimeout` to update the `metric`
    every second.

--- app/data/metrics/Cosine.ts
+++ app/data/metrics/Cosine.ts
@@ -1,12 +1,14 @@
 import type { Metric, MetricDatum } from './types'
 import secondsInRange from "~/util/secondsInRange"
-
 export default class Cosine implements Metric {
   name = 'Cosine'
-
+  start: number
+  end: number
   data: Array<MetricDatum>
 
   constructor(start: number, end: number) {
+    this.start = start
+    this.end = end
     this.data = secondsInRange(start, end)
       .map(timestamp => ({ timestamp, value: Math.cos(timestamp) }))
   }
