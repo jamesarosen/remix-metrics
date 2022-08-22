d3767f7d2d4bf8f790d2df14a2dca6a7b65348a4
James A Rosen <jamesr@everlane.com>
2022-08-22 08:54:21 -0700

    ✨(Footer) add link to GitHub repo

--- app/components/SiteFooter.tsx
+++ app/components/SiteFooter.tsx
@@ -5,7 +5,11 @@ export default function SiteFooter(props: any) {
       <a href="https://jamesarosen.com" target="_blank" rel="noreferrer">
         James A Rosen
       </a>
-      .
+      <br />
+      Source on{" "}
+      <a href="https://github.com/jamesarosen/remix-metrics" target="_blank" rel="noreferrer">
+        GitHub
+      </a>
       <br />
       <a
         href="https://thenounproject.com/icon/sine-4900361/"
