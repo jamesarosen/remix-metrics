f7f67ddc74721ebde45ca4d651e7decfe225e409
James A Rosen <james.a.rosen@gmail.com>
2022-08-21 19:34:14 -0700

    🍱 use Sine wave favicon from Noun Project

--- app/components/SiteFooter.tsx
+++ app/components/SiteFooter.tsx
@@ -5,7 +5,15 @@ export default function SiteFooter() {
       <a href="https://jamesarosen.com" target="_blank" rel="noreferrer">
         James A Rosen
       </a>
-      .
+      .{" "}
+      <a
+        href="https://thenounproject.com/icon/sine-4900361/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Sine icon
+      </a>{" "}
+      by Zach Bogart from The Noun Project
     </footer>
   );
 }
