23824d980008e417595f1593269d43859289ba38
James A Rosen <jamesr@everlane.com>
2022-08-21 18:50:12 -0700

    ✨ add healthcheck endpoint

---
+++ app/routes/healthcheck.ts
@@ -0,0 +1,9 @@
+export async function loader() {
+  return new Response("OK", {
+    status: 200,
+    headers: {
+      "Cache-Control": "no-store, no-transform",
+      "Content-Type": "text/plain",
+    },
+  });
+}
